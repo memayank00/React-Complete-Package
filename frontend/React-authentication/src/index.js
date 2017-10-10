58356f541edcebab9d5d53b96578db5232b87a90
Mayank <mayank_singh@seologistics.com>
2017-10-10 18:07:05 +0530

    first commit

---
+++ frontend/React-authentication/src/index.js
@@ -0,0 +1,36 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CelebrityJokes from './components/CelebrityJokes';
+import FoodJokes from './components/FoodJokes';
+//import { Router, Route, browserHistory } from 'react-router';
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Root = () => {
+
+  return (
+    
+      <BrowserRouter >
+      <div className="container">
+        <Route path="/" component={FoodJokes}/>
+        <Route path="/special" component={CelebrityJokes}/>
+       </div>
+      </BrowserRouter>
+
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
